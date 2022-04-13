9c604e1dcf7e317b0ff8c2b48ee77c24f86d7957
Jonathan Choi <jonchoi11@gmail.com>
2022-04-13 14:37:41 -0600

    add toggle state and import motion

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { HiMenuAlt4, HiX } from 'react-icons/hi';
+import { motion } from 'framer-motion';
 
 import "./Navbar.scss";
 import { images } from "../../constants";
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+
   return (
     <nav className="app__navbar">
       <div className="app__navbar-logo">
@@ -17,6 +21,20 @@ const Navbar = () => {
           </li>
         ))}
       </ul>
+
+      <div className="app__navbar-menu">
+          <HiMenuAlt4 onClick={() => setToggle(true)}/>
+
+          {
+            toggle && (
+              <motion.div
+              whileInView={{ x: [300, 0]}}
+              transition={{ duration: 0.85, ease: 'easeOut'}}
+              >
+
+              </motion.div>
+            )}
+      </div>
     </nav>
   )
 }
