c043050233bc2ffdd5a27ccf65669f8e2f8b55f5
Jonathan Choi <jonchoi11@gmail.com>
2022-05-07 13:30:27 -0600

    add new logo for navbar

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -44,7 +44,7 @@ const Header = () => {
         transition={{ duration: 0.5, delayChildren: 0.5}}
         className="app__header-img"
       >
-        <img style={{ width: 400, height: 500 }} src={images.profile} alt="profile_bg"/>
+        <img  src={images.profile} alt="profile_bg"/>
         <motion.img
           whileInView={{ scale: [0, 1]}}
           transition={{ duration: 1, ease: 'easeInOut' }}
@@ -59,9 +59,9 @@ const Header = () => {
         whileInView={scaleVariants.whileInView}
         className="app__header-circles"
       >
-        {[images.flutter, images.redux, images.sass].map((circle, index) => (
+        {[images.react, images.javascript, images.sass].map((circle, index) => (
           <div clasName="circle-cmp app__flex" key={`circle-${index}`}>
-            <img src={circle} alt="circle" />
+            <img src={circle} alt="profile_bg" />
           </div>
         ))}
       </motion.div>
