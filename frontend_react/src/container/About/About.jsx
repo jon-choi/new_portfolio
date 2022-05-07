194fa4f0647777a8a4d1d801fba44702249a50a7
Jonathan Choi <jonchoi11@gmail.com>
2022-05-07 14:58:33 -0600

    work on about

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 
-import { images } from '../../constants';
 import "./About.scss";
 import { urlFor, client } from '../../client';
 
@@ -13,8 +12,9 @@ const About = () => {
   useEffect(() => {
     const query = '*[_type == "abouts"]';
 
-    client.fetch(query)
-    .then((data) => setAbouts(data))
+    client.fetch(query).then((data) => {
+      setAbouts(data);
+    });
   }, []);
   
 
@@ -23,23 +23,22 @@ const About = () => {
       <h2 className="head-text">First, <span>solve the problem </span><br />then, <span>write the code</span></h2>
         <h3 style={{ alignItems: 'center', justifyContent: 'center', display: 'flex' }}><em>- John Johnson</em></h3>
         <div className="app__profiles">
-          {abouts.map((about, index) => (
-            <motion.div
-              whileInView={{ opacity: 1 }}
-              whileHover={{ scale: 1.1 }}
-              transition={{ duration: 0.5, type: 'tween' }}
-              className="app__profile-item"
-              key={about.title + index}
-            >
-              <img src={about.imgUrl} alt={about.title} />
-              <h2 className="bold-text" style={{marginTop: 20}}>{about.title}</h2>
-              <h2 className="p-text" style={{marginTop: 10}}>{about.description}</h2>
-            </motion.div>
-          ))}
-
-        </div>
+        {abouts.map((about, index) => (
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: 'tween' }}
+            className="app__profile-item"
+            key={about.title + index}
+          >
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
+            <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
+            <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
+          </motion.div>
+        ))}
+      </div>
     </>
   )
 }
 
-export default About
+export default About;
