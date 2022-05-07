ab58adb5e5dca6027cca0941ebf0bc8ec489d105
Jonathan Choi <jonchoi11@gmail.com>
2022-05-07 16:41:26 -0600

    remove ux/ui filter

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -46,7 +46,7 @@ const Work = () => {
       <h2 className="head-text"><span>Projects</span></h2>
 
       <div className="app__work-filter">
-        {['UI/UX', 'Web App', 'Mobile App', 'React JS', 'All'].map((item, index) => (
+        {['Web App', 'Mobile App', 'React JS', 'All'].map((item, index) => (
           <div
             key={index}
             onClick={() => handleWorkFilter(item)}
