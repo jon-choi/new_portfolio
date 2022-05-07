6fc8f2bb85d34c568fa18f67cc46e3e452e533c9
Jonathan Choi <jonchoi11@gmail.com>
2022-05-07 15:57:38 -0600

    create work/project component

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -1,10 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { AiFillEye, AiFillGithub } from 'react-icons/ai';
+import { motion } from 'framer-motion';
+
+import { AppWrap } from '../../wrapper';
+
+import { urlFor, client } from '../../client';
 
 import "./Work.scss";
 
 const Work = () => {
+
+  const [activeFilter, setActiveFilter] = useState('All');
+  const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1});
+
+  const handleWorkFilter = (item) => {
+
+  }
+
   return (
-    <div>Work</div>
+    <>
+      <h2 className="head-text"><span>Projects</span></h2>
+
+      <div className="app__work-filter">
+        {['Web App', 'Mobile App', 'React JS', 'All'].map((item, index) => (
+          <div
+            key={index}
+            onClick={() => handleWorkFilter(item)}
+            className={`app__work-filter-icon app__flex p-text ${activeFilter === item ? 'item-active' : ''}`}
+          >
+            {item}
+          </div>
+        ))}
+      </div>
+
+      <motion.div
+        animate={animateCard}
+        transition={{ duration: 0.5, delayChildren: 0.5}}
+        className="app__work-portfolio"
+      >
+        
+      </motion.div>
+    </>
   )
 }
 
