d46c11ff066b15a4de9718cb5dadb0f7a32b3edc
Jonathan Choi <jonchoi11@gmail.com>
2022-05-09 13:51:53 -0600

    change sass icon to html in header circles

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -61,7 +61,7 @@ const Header = () => {
         whileInView={scaleVariants.whileInView}
         className="app__header-circles"
       >
-        {[images.react, images.javascript, images.sass].map((circle, index) => (
+        {[images.react, images.javascript, images.html].map((circle, index) => (
           <div className="circle-cmp app__flex" key={`circle-${index}`}>
             <img src={circle} alt="profile_bg" />
           </div>
