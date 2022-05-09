8ff1cfc91ee807876e16134a2b6ae00c9e446916
Jonathan Choi <jonchoi11@gmail.com>
2022-05-09 14:25:30 -0600

    use nested map to fetch work experience data from sanity

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -35,7 +35,7 @@ const Skills = () => {
 
       <div className="app__skills-container">
         <motion.div className="app__skills-list">
-          {skills.map((skill) => (
+          {skills?.map((skill) => (
             <motion.div
               whileInView={{opacity: [0, 1]}}
               transition={{ duration: 0.5}}
@@ -48,6 +48,47 @@ const Skills = () => {
               <p className="p-text">{skill.name}</p>
             </motion.div>
           ))}
+        </motion.div>
+
+        <motion.div className="app__skills-exp">
+          {experience.map((experience) => (
+            <motion.div
+              className="app__skills-exp-item"
+              key={experience.year}
+            >
+              <div className="app__skills-exp-year">
+                <p className="bold-text">{experience.year}</p>
+              </div>
+              <motion.div className="app__skills-exp-works">
+                {experience.works.map((work) => (
+                  <>
+              <motion.div
+                whileInView={{opacity: [0, 1]}}
+                transition={{ duration: 0.5}}
+                className="app__skills-exp-work"
+                data-tip
+                data-for={work.name}
+                key={work.name}
+              >
+                <h4 className="bold-text">{work.name}</h4>
+                <p className="p-text">{work.company}</p>
+              </motion.div>
+              <ReactTooltip
+                id={work.name}
+                effect="solid"
+                arrowColor="#fff"
+                className="skills-tooltip"
+              >
+                {work.desc}
+              </ReactTooltip>
+            </>
+                ))}
+              </motion.div>
+
+            </motion.div>
+            
+          ))}
+
         </motion.div>
       </div>
     </>
