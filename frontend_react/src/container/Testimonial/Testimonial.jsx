b17b8e0efd043684ae48cb4de9a73d327bd96348
Jonathan Choi <jonchoi11@gmail.com>
2022-05-09 15:12:44 -0600

    import dependencies for testimonial component and store state

--- frontend_react/src/container/Testimonial/Testimonial.jsx
+++ frontend_react/src/container/Testimonial/Testimonial.jsx
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
+import { HiChevronLeft, HiChevronRight } from 'react-icons/hi';
+
+import { AppWrap, MotionWrap } from '../../wrapper';
+
+import { urlFor, client } from '../../client';
 
 import "./Testimonial.scss";
 
 const Testimonial = () => {
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [testimonials, setTestimonials] = useState(0);
+  const [brands, setBrands] = useState([]);
+
+  const handleClick = (index) => {
+    setCurrentIndex(index);
+  }
+
   return (
     <div>Testimonial</div>
   )
