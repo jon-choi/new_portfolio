87ad13c9f3350c5818a1e3f605b49e414b52cc7e
Jonathan Choi <jonchoi11@gmail.com>
2022-05-09 15:14:28 -0600

    export motion wrap for testimonial

--- frontend_react/src/container/Testimonial/Testimonial.jsx
+++ frontend_react/src/container/Testimonial/Testimonial.jsx
@@ -36,4 +36,8 @@ const Testimonial = () => {
   )
 }
 
-export default Testimonial
+export default AppWrap(
+  MotionWrap(Testimonial, 'app__testimonial'),
+  'testimonial',
+  'app__primarybg',
+);
