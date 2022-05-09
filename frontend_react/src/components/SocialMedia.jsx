4c5a102e0cdf0549dad3f37769e3f899eca30621
Jonathan Choi <jonchoi11@gmail.com>
2022-05-09 17:36:09 -0600

    change social icons

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
-import { BsTwitter, BsInstagram } from 'react-icons/bs';
-import { FaFacebookF } from 'react-icons/fa';
+import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
+import { SiVsco, SiMedium } from 'react-icons/si';
 
 const SocialMedia = () => (
   <div className="app__social">
     <div>
-      <BsTwitter />
+      <AiFillGithub />
     </div>
     <div>
-      <FaFacebookF />
+      <AiFillLinkedin />
     </div>
     <div>
-      <BsInstagram />
+      <SiVsco />
+    </div>
+    <div>
+      <SiMedium />
     </div>
   </div>
 );
